ab678ceac9c1e9446b302c846ac8767fb0b07440
DigilabsAI <devajofdigilabs@gmail.com>
2026-01-12 21:34:31 +0800

    feat: add settings profile component with avatar upload and email change functionality
    feat: implement input group component for better input handling
    
    feat: create progress component for visual task tracking
    
    feat: develop project list component with filtering and project management features

--- app/(app)/protected/page.tsx
+++ app/(app)/protected/page.tsx
@@ -21,20 +21,16 @@ export default function ProtectedPage() {
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
+          Site Under Development. Some features may not work as expected.
         </div>
       </div>
       <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
+        <h2 className="font-bold text-2xl mb-4"> User details</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           <Suspense>
             <UserDetails />
           </Suspense>
         </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
       </div>
     </div>
   );
